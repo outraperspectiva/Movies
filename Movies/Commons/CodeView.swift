738457a6dd6cc790e7c11d657b4fdf64a88beb41
outraperspectiva <fabio.fernandez@ifb.edu.br>
2025-05-25 00:38:21 -0300

    layout

---
+++ Movies/Commons/CodeView.swift
@@ -0,0 +1,8 @@
+//
+//  CodeView.swift
+//  Movies
+//
+//  Created by Fabio Fernandez on 24/05/25.
+//
+
+import Foundation
---
+++ Movies/Commons/UIViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController.swift
+//  Movies
+//
+//  Created by Fabio Fernandez on 24/05/25.
+//
+
+import Foundation
